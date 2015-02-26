b0b9f688069d4143f1a9d536f43658be04937cb9
Puru Dahal <pdahal@me.com>
2015-02-25 22:24:08 -0800

    Remove before_action on base controller

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,7 +1,6 @@
 class Api::BaseController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
-  before_action :authenticate!
   helper_method :current_user
 
   # /v1 (All Endpoints)
